8e3aa2be36a00365bc950ffeb180bda25d6d0fd5
kulkarnisankalp598-png <kulkarnisankalp598@gmail.com>
2025-12-27 20:03:14 -0600

    import random allows questions to be shuffled and I also implemented the questions that will be asked on the quiz

---
+++ game.py
@@ -0,0 +1,25 @@
+import random
+
+#These are the Quiz questions
+quiz = [
+    {
+        "question": "What is 2 + 2?",
+        "options": ["1", "2", "3", "4"],
+        "answer": "4"
+    },
+    {
+        "question": "What is the capital of France?",
+        "options": ["Berlin", "London", "Paris", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Who is the greatest basketball player ever?",
+        "options": ["Tom Brady", "Stephen Curry", "Alex Caruso", "Lebron James"],
+        "answer": "Lebron James"
+    },
+    {
+        "question": "What does CPU stand for?",
+        "options": ["Central Processing Unit", "Computer Power Unit", "Central Program Unit", "Control Processing Unit"],
+        "answer": "Central Processing Unit"
+    }
+]
